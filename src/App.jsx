d5b293f2bb9f0638f4092cf8fb2624084af4a59c
Arya <arya12345kishan@gmail.com>
2024-02-17 22:35:37 +0530

    speech

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Homepage from './Pages/Homepage/Homepage'
 import Detail from './Pages/Detail/Detail'
 import Speech from './Pages/Speech/Speech'
@@ -8,9 +8,9 @@ export default function App() {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Homepage/>} />
-        <Route path='/detail/:globe' element={<Detail/>} />
-        <Route path='/speech' element={<Speech/>} />
+        <Route path='/' element={<Homepage />} />
+        <Route path='/detail/:globe' element={<Detail />} />
+        <Route path='/speech' element={<Speech />} />
       </Routes>
     </div>
   )
--- src/Pages/Speech/Speech.jsx
+++ src/Pages/Speech/Speech.jsx
@@ -1,14 +1,24 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import "./Speech.scss"
 import 'regenerator-runtime/runtime'
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
 import mic from '../../assets/mic.png'
+import listenGif from "../../assets/listen.gif"
 
 export default function Speech({ fetchData, input }) {
 
-    const { transcript, browserSupportsSpeechRecognition } = useSpeechRecognition();
+    const { transcript, browserSupportsSpeechRecognition, finalTranscript, listening } = useSpeechRecognition();
+    const ref = useRef()
 
-    const startListening1 = () => SpeechRecognition.startListening();
+    const startListening1 = () => {
+
+        SpeechRecognition.startListening({
+            continuous: false,
+            language: 'en-GB',
+        })
+
+        ref.current.style.display = "flex"
+    };
 
 
     if (!browserSupportsSpeechRecognition) {
@@ -20,9 +30,23 @@ export default function Speech({ fetchData, input }) {
         input.current.focus();
     }
 
+
+    useEffect(() => {
+        if (finalTranscript !== '') {
+            console.log('Got final result:', finalTranscript);
+            fetchData(finalTranscript)
+            ref.current.style.display = "none"
+        }
+
+
+    }, [finalTranscript]);
+
     return (
-        <div className='speech'>
-            <img onClick={startListening1} src={mic} alt="" srcSet="" />
-        </div>
+        <>
+            <div className='speech'>
+                <img onClick={startListening1} src={mic} alt="" srcSet="" />
+            </div>
+            <img className='listenGirl' ref={ref} src={listenGif} alt="" srcset="" />
+        </>
     )
 }
